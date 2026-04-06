3977e2f4a321b9d031a959da758868ae4406d749
Alex4747-J <npanh1903@gmail.com>
2026-04-05 22:58:09 -0400

    Update ArtifactFactory.php

--- database/factories/ArtifactFactory.php
+++ database/factories/ArtifactFactory.php
@@ -16,9 +16,15 @@ class ArtifactFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+{
+    return [
+        'name' => fake()->unique()->words(3, true),
+        'object_type' => fake()->randomElement(['Helmet', 'Uniform', 'Weapon', 'Medal', 'Equipment', 'Document']),
+        'period' => fake()->randomElement(['WWI', 'WWII', '1950s', 'Cold War', 'Korean War']),
+        'origin' => fake()->randomElement(['RAF', 'RCAF', 'Canadian Army', 'Royal Navy', 'US Air Force']),
+        'material' => fake()->randomElement(['Leather', 'Steel', 'Wood', 'Brass', 'Cotton', 'Paper']),
+        'description' => fake()->paragraph(),
+        'image_path' => null,
+    ];
+}
 }
