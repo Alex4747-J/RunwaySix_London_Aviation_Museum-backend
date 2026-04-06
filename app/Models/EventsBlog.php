0ad371688289590cb3d92f0346c3b8e0cbff9e2f
Alex4747-J <npanh1903@gmail.com>
2026-04-06 08:20:55 -0400

    Update EventsBlog Model
    Update the model to use hasfactory

--- app/Models/EventsBlog.php
+++ app/Models/EventsBlog.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EventsBlog extends Model
 {
-    use SoftDeletes;
+    use HasFactory ,SoftDeletes;
 
     protected $table = 'events_blog';
 
