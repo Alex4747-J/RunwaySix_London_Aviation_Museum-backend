30b6f55cb86060e1a2f071de39421e290dcb3d7f
Alex4747-J <npanh1903@gmail.com>
2026-04-05 23:17:12 -0400

    Update: Artifacts Routes to api.php
    Update the Artifacts Routes for each controller in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\Api\ArtifactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+/**
+ * Artifacts API Routes
+ */
+
+Route::get('/artifacts', [ArtifactController::class, 'index']);
+Route::post('/artifacts', [ArtifactController::class, 'store']);
+Route::get('/artifacts/{artifact}', [ArtifactController::class, 'show']);
+Route::patch('/artifacts/{artifact}', [ArtifactController::class, 'update']);
+Route::delete('/artifacts/{artifact}', [ArtifactController::class, 'destroy']);
