0bb46c27133530632e8ce304c25aaa2b734de169
Alex4747-J <npanh1903@gmail.com>
2026-04-07 07:11:45 -0400

    Add Artifact and Remembrance seeders
    Register ArtifactSeeder and RemembranceSeeder in DatabaseSeeder so their seed data is executed when running database seeds (in addition to EventsBlogSeeder). This ensures artifact and remembrance records are populated during seeding.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             EventsBlogSeeder::class,
+            ArtifactSeeder::class,
+            RemembranceSeeder::class,
         ]);
     }
 }
